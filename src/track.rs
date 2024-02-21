29c19347a32613f87da1d1fd5530c1bc90c0c22b
Randoragon <randoragongamedev@gmail.com>
2024-02-21 20:49:46 +0100

    docs(track): Add note about Track needing to stay lightweight

--- src/track.rs
+++ src/track.rs
@@ -1,6 +1,11 @@
 use camino::{Utf8Path, Utf8PathBuf};
 
 /// A track in a playlist.
+///
+/// Note that this struct should only provide basic path information for general-purpose use, and
+/// otherwise be fast to hash, clone and not take up a lot of memory. If more information is
+/// needed, such as file metadata, ID3v2 tags, etc., it should be delegated to a separate place in
+/// memory, to keep this lightweight.
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct Track {
     /// The path to the audio file, relative to `MUSIC_DIR`.
