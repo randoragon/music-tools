6740ee220b0b79ff114caeefb40c13f78ec0533b
randoragon <randoragongamedev@gmail.com>
2024-07-13 22:18:32 +0200

    fix(track): Trim paths to be relative to MUSIC_DIR
    This is only done in obvious cases. Generally speaking, Track should be
    a low-level structure and not do a lot of strict checking, because it
    should provide flexibility for higher-level functions.
    
    I slightly rephrased the documentation to emphasize the fact that Tracks
    must be unique within a playlist. This of course is circumvented in the
    case of Playcounts, which have a wrapper Entry structure, but the idea
    is that Tracks are what you use when you decide which part of a playlist
    you want to remove, etc.

--- src/track.rs
+++ src/track.rs
@@ -1,8 +1,9 @@
+use crate::music_dir;
 use camino::{Utf8Path, Utf8PathBuf};
 
 /// A track in a playlist.
 ///
-/// Note that this struct should only provide basic path information for general-purpose use, and
+/// Note that this struct should only provide basic path information for unique identification, and
 /// otherwise be fast to hash, clone and not take up a lot of memory. If more information is
 /// needed, such as file metadata, ID3v2 tags, etc., it should be delegated to a separate place in
 /// memory, to keep this lightweight.
@@ -13,9 +14,17 @@ pub struct Track {
 }
 
 impl Track {
+    /// If `fpath` begins with `MUSIC_DIR`, the prefix is truncated, leaving a relative path.
     pub fn new<T: AsRef<Utf8Path>>(fpath: T) -> Self {
         Track {
-            path: Utf8PathBuf::from(fpath.as_ref()),
+            path: Utf8PathBuf::from(
+                // Strip music_dir prefix, if it exists
+                if fpath.as_ref().starts_with(music_dir()) {
+                    fpath.as_ref().strip_prefix(music_dir()).unwrap_or(fpath.as_ref())
+                } else {
+                    fpath.as_ref()
+                }
+            ),
         }
     }
 }
