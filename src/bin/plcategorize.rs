b216270ee867a98467cae1be20d728ae6b629dbf
randoragon <randoragongamedev@gmail.com>
2025-01-14 12:31:10 +0100

    feat(plcategorize): Keyboard handling boilerplate

--- src/bin/plcategorize.rs
+++ src/bin/plcategorize.rs
@@ -1,4 +1,4 @@
-use crossterm::event::{self, Event};
+use crossterm::event::{self, Event, KeyCode};
 use ratatui::{
     text::Text,
     widgets::Clear,
@@ -65,6 +65,50 @@ fn draw(app: &App, frame: &mut Frame) {
     frame.render_widget(TuiPicker::new(&app.picker_state, ""), layout[2]);
 }
 
+enum Action {
+    Quit,
+    NewChar,
+    Refresh,
+    ClearInput,
+    Ignore,
+}
+
+/// Handles a crossterm event.
+///
+/// Return values:
+/// - 0: quit application
+/// - 1: default (add to input buffer)
+/// - 2: refresh UI
+/// - 3: clear input
+fn handle_event(ev: Event, input: &mut String) -> Action {
+    match ev {
+        Event::Key(kev) => handle_key_event(kev, input),
+        _ => Action::Ignore,
+    }
+}
+
+fn handle_key_event(kev: event::KeyEvent, input: &mut String) -> Action {
+    if kev.code == KeyCode::Char('q') && input.is_empty() {
+        return Action::Quit;
+    }
+
+    if kev.code == KeyCode::Esc {
+        if !input.is_empty() {
+            return Action::ClearInput;
+        } else {
+            return Action::Refresh;
+        }
+    }
+
+    match kev.code {
+        KeyCode::Char(c) => {
+            input.push(c);
+            Action::NewChar
+        },
+        _ => Action::Ignore,
+    }
+}
+
 fn main() -> ExitCode {
     stderrlog::new()
         .module(module_path!())
@@ -80,14 +124,45 @@ fn main() -> ExitCode {
             return ExitCode::FAILURE;
         },
     };
+    let mut input = String::with_capacity(32);
     let mut terminal = ratatui::init();
     loop {
         app = update(app);
-        terminal.draw(|x| draw(&app, x)).expect("failed to draw frame");
-        if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
-            break;
+        if let Err(e) = terminal.draw(|x| draw(&app, x)) {
+            error!("Failed to draw frame: {e}");
+            return ExitCode::FAILURE;
+        }
+
+        // Event handling
+        let ev = match event::read() {
+            Ok(ev) => ev,
+            Err(e) => {
+                error!("Failed to read event: {e}");
+                return ExitCode::FAILURE;
+            }
+        };
+
+        match handle_event(ev, &mut input) {
+            Action::Ignore => {},
+            Action::Quit => break,
+            Action::NewChar => {
+                // Check input, if it matches any playlist, toggle that playlist and clear input.
+                // TODO
+
+                // If input does not match the beginning of any playlist shortcut, clear it.
+                // TODO
+            },
+            Action::Refresh => {
+                // TODO
+            }
+            Action::ClearInput => {
+                input.clear();
+                // TODO: erase shortcut highlights
+            },
         }
     }
+
+    // Exit
     ratatui::restore();
     ExitCode::SUCCESS
 }
